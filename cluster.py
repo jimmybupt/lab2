20b6c2814724c850e219c4c378c2a5f266a7d539
lkterry0301 <terryliu0301@gmail.com>
2016-02-22 16:06:26 -0500

    return prediction for quality evaluation

--- cluster.py
+++ cluster.py
@@ -4,10 +4,12 @@ from sklearn.cluster import AgglomerativeClustering
 #KMeans clustering function
 def kmeans_clustering(n_clusters,word_vector):
   model=KMeans(n_clusters)
-  return model.fit_predict(word_vector)
+  prediction=model.fit_predict(word_vector)
+  return prediction
   
 #Hierarchical clustering function
 def hierarchical_clustering(n_clusters,word_vector):
   model=AgglomerativeClustering(n_clusters)
-  return model.fit_predict(word_vector)
+  prediction=model.fit_predict(word_vector)
+  return prediction
 
