a4f7457a8eae882b5bc22764609956f58cc94236
lkterry0301 <terryliu0301@gmail.com>
2016-02-22 15:54:26 -0500

    two clustering functions

--- cluster.py
+++ cluster.py
@@ -1,2 +1,13 @@
-print 'Cluster Lab'
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+
+#KMeans clustering function
+def kmeans_clustering(n_clusters,word_vector):
+  model=KMeans(n_clusters)
+  return model.fit_predict(word_vector)
+  
+#Hierarchical clustering function
+def hierarchical_clustering(n_clusters,word_vector):
+  model=AgglomerativeClustering(n_clusters)
+  return model.fit_predict(word_vector)
 
