e5b5d80ce9ac00713362df6a43972d929e988087
lkterry0301 <terryliu0301@gmail.com>
2016-02-22 15:11:16 -0500

    add euclidean distance and cosine similarity

--- distance.py
+++ distance.py
@@ -2,4 +2,12 @@ def euclidean_sparse(sparse_v1, sparse_v2):
 	sum = 0.0
 	return sum
 
+from scipy.spatial import distance
+#euclidean distance between one pair of vectors
+def euclidean_distance(v1,v2):
+	return distance.euclidean(v1,v2)
 
+#cosine similarity between one pair of vectors
+def cosine_similarity(v1,v2):
+	return 1-distance.cosine(v1,v2)
+	
