adf317fad39487372a68d8a9527e06979c39e4bc
lkterry0301 <terryliu0301@gmail.com>
2016-02-22 22:32:02 -0500

    linkage option for hierarchical

--- cluster.py
+++ cluster.py
@@ -3,13 +3,13 @@ from sklearn.cluster import AgglomerativeClustering
 
 #KMeans clustering function
 def kmeans_clustering(n_clusters,word_vector):
-  model=KMeans(n_clusters)
+  model=KMeans(n_clusters=n_clusters)
   prediction=model.fit_predict(word_vector)
   return prediction
   
 #Hierarchical clustering function
 def hierarchical_clustering(n_clusters,word_vector):
-  model=AgglomerativeClustering(n_clusters)
+  model=AgglomerativeClustering(n_clusters=n_clusters,linkage='ward') #linkage can be {“ward”, “complete”, “average”}
   prediction=model.fit_predict(word_vector)
   return prediction
 
